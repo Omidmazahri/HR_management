7f9ba3f1395498c4b6eae8d5e63482f77ddffec8
omid mazaheri <om.id1014@outlook.com>
2023-01-12 18:52:30 +0330

    add base folders

---
+++ HR_Management/HR_Management.Domain/Common/BaseDomainEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_Management.Domain.Common
+{
+    public abstract class BaseDomainEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime ModifyDate { get; set; }
+        public string ModifyBy { get; set; }
+
+
+    }
+}
